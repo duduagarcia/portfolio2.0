27e4216979e61ec135182154d60d3640d451cbd3
duduagarcia <eduardo.agarcia117@gmail.com>
2024-01-25 18:50:37 -0300

    feat: setted up lenis and gsap scrollsmother

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,24 @@
 <script setup>
+import Lenis from "@studio-freight/lenis";
+const { gsap, ScrollTrigger, ScrollToPlugin } = useGsap();
+
 const { locale } = useI18n();
 
+onMounted(() => {
+  const lenis = new Lenis();
+
+  // lenis.on("scroll", (e) => {
+  //   console.log(e);
+  // });
+
+  lenis.on("scroll", ScrollTrigger.update);
+
+  gsap.ticker.add((time) => {
+    lenis.raf(time * 1000);
+  });
+
+  gsap.ticker.lagSmoothing(0);
+});
 useHead({
   title: "Eduardo Garcia · Full Stack Developer",
   meta: [
