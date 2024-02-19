4dbe6643f9d14934b33ecec3a1e0601d6b8d7552
duduagarcia <eduardo.agarcia117@gmail.com>
2024-02-19 14:06:58 -0300

    feat: responsive about layout

--- components/sections/About.vue
+++ components/sections/About.vue
@@ -1,9 +1,30 @@
 <script setup></script>
 
 <template>
-  <div>
+  <div style="position: relative">
     <BG color="light" />
-    <div class="container"><p>About</p></div>
+    <div class="container">
+      <div class="col1">
+        <div class="img_wrapper"></div>
+      </div>
+      <div class="col2">
+        <h2>Passionate developer<br />based in Brazil</h2>
+        <p>
+          I'm student pushing limits and chasing excellence. I'm rarely
+          satisfied with my own work, but this end up being what makes me really
+          good.
+        </p>
+        <p>
+          When i'm not building or exploring new web experiences, i'm probably
+          playing games.
+        </p>
+        <p>
+          Currently studying for a Bachelor's degree in Software Engineering at
+          PUCRSV
+        </p>
+        <a href="#">Download Resume</a>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -11,7 +32,70 @@
 .container {
   position: relative;
   z-index: 2;
-  height: 100vh;
-  border: 1px solid salmon;
+  height: max-content;
+  padding: 120px 20px;
+
+  display: grid;
+  grid-template-areas: "col1 col2";
+  grid-template-columns: 1fr 1fr;
+}
+
+.col1 {
+  grid-area: col1;
+}
+
+.col1 .img_wrapper {
+  max-width: 500px;
+  height: 600px;
+  background-color: #c6c6c6;
+}
+
+.col2 {
+  grid-area: col2;
+  padding: 20px 20px;
+}
+
+.col2 h2 {
+  font-size: 36px;
+  margin-bottom: 20px;
+}
+
+.col2 p {
+  font-size: 18px;
+  margin-bottom: 15px;
+}
+
+@media screen and (max-width: 800px) {
+  .container {
+    grid-template-areas: "col1" "col2";
+    grid-template-rows: 1fr 1fr;
+    grid-template-columns: 1fr;
+  }
+  .col1 .img_wrapper {
+    max-width: 100%;
+    height: 100%;
+  }
+
+  .col2 {
+    grid-area: col2;
+    padding: 20px 10px;
+  }
+}
+
+@media screen and (max-width: 500px) {
+  .container {
+    display: flex;
+    flex-direction: column;
+    padding: 150px 0;
+  }
+
+  .col1 .img_wrapper {
+    width: 100%;
+    height: 400px;
+  }
+
+  .col2 {
+    padding: 20px;
+  }
 }
 </style>
